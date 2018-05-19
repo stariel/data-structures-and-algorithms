8ce3f74f082968d73eba7b2ad7e3a6ed608774ef
Ariel Altaras <ariel.altaras@gmail.com>
2018-05-19 14:20:08 -0700

    wrote arrayReverse function

---
+++ arrays/array_reverse/array_reverse.js
@@ -0,0 +1,8 @@
+function reverseArray(arr) {
+    const newArray = [];
+      for (let i = (arr.length - 1); i >= 0; i--) {
+          let val = arr[i];
+          newArray.push(val);
+      }
+      console.log(newArray)
+  }

ecb5e4e226da5f951bb5a15937a5097999583a67
Ariel Altaras <ariel.altaras@gmail.com>
2018-05-21 23:58:48 -0700

    working on testing

--- array_binary_search.test.js
+++ array_binary_search.test.js
@@ -13,3 +13,15 @@ it('BinarySearch() returns null if it is passed something other than an array an
   expect(BinarySearch([1,2,3])).toEqual(null);
         
 });
+
+it('BinarySearch() searches for the index of the element in the array that matches the key and returns the index or -1 if a element matching the key is not present.', () => {
+
+  expect(BinarySearch([4,8,15,16,23,42], 15)).toEqual(2);
+            
+});
+
+it('BinarySearch() searches for the index of the element in the array that matches the key and returns the index or -1 if a element matching the key is not present.', () => {
+
+  expect(BinarySearch([11,22,33,44,55,66,77], 90)).toEqual(-1);
+              
+});

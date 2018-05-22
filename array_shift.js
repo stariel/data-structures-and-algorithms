fd27bdfb75129c93c15670b3ab46c28b977828b1
Ariel Altaras <ariel.altaras@gmail.com>
2018-05-21 22:45:34 -0700

    wrote tests and insertShiftArray function

--- array_shift.js
+++ array_shift.js
@@ -1,10 +1,21 @@
 'use strict';
 
 function insertShiftArray(array, value) {
-let arrLength = array.length;
-let centerIndex = Math.round(arrLength);
+  let arrLength = array.length;
+  let centerIndex = Math.ceil(arrLength/2);
+  let newArr = [];
   if ((typeof array !== 'object') || (typeof value == 'undefined')) {
     return null;
+  }
+  else {
+    for (let i = 0; i < centerIndex; i++) {
+      newArr[i] = array[i];
+    }
+    newArr[centerIndex] = value;
+    for (let j = (centerIndex); j < (array.length); j++) {
+      newArr[j+1] = array[j];
+    }
+    return newArr;
   }
 }
 
--- array_shift.test.js
+++ array_shift.test.js
@@ -4,6 +4,24 @@ let insertShiftArray = require('./array_shift.js');
 
 it('insertShiftArray() returns null if it is passed something other than an array and a value', () => {
 
-  expect(insertShiftArray('kitty')).toEqual(null);
+  expect(insertShiftArray('kitty', null)).toEqual(null);
       
+});
+
+it('insertShiftArray() returns null if it is passed something other than an array and a value', () => {
+
+  expect(insertShiftArray([1,2,3])).toEqual(null);
+        
+});
+
+it('insertShiftArray() inserts the given value at the center index of the new array and returns it', () => {
+
+  expect(insertShiftArray([2,4,6,8], 5)).toEqual([2,4,5,6,8]);
+          
+});
+
+it('insertShiftArray() inserts the given value at the center index of the new array and returns it', () => {
+
+  expect(insertShiftArray([4,8,15,23,42], 16)).toEqual([4,8,15,16,23,42]);
+            
 });

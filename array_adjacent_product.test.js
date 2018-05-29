ed5184779bbe8e5472a997df17c9db24a24435bd
Ariel Altaras <ariel.altaras@gmail.com>
2018-05-28 17:40:25 -0700

    tests and function working

--- array_adjacent_product.test.js
+++ array_adjacent_product.test.js
@@ -7,3 +7,35 @@ it('LargestProduct() returns null if it is passed something other than an array'
   expect(LargestProduct('hello', 5)).toEqual(null);
       
 });
+
+it('LargestProduct() returns the product of two adjacent values', () => {
+  let input = [1,2];
+  let actual = LargestProduct(input);
+
+  expect(actual).toBe(2);
+        
+});
+
+it('LargestProduct() returns the product of two adjacent values in a 2D array', () => {
+  let input = [[1,1],[2,1]];
+  let actual = LargestProduct(input);
+  
+  expect(actual).toBe(2);
+          
+});
+
+it('LargestProduct() returns the product of two adjacent values in a 2D array', () => {
+  let input = [[1,2,3],[4,5,6]];
+  let actual = LargestProduct(input);
+    
+  expect(actual).toBe(30);
+            
+});
+
+it('LargestProduct() returns the product of two adjacent values in a 2D array', () => {
+  let input = [[1,2,1],[2,4,2],[3,6,8],[7,8,1]];
+  let actual = LargestProduct(input);
+      
+  expect(actual).toBe(64);
+              
+});

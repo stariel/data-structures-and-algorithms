9318ffd2bd543cea3082790063811bc68d3236dd
Ariel Altaras <ariel.altaras@gmail.com>
2018-06-11 19:39:55 -0700

    100% test coverage and passing for first three functions

--- linked-lists/__tests__/index.test.js
+++ linked-lists/__tests__/index.test.js
@@ -8,66 +8,67 @@ describe('Linked LinkedList Constructor', () => {
     list.append(4);
     expect(list.head.value).toEqual(4);
   });
-});
 
-it('append() attaches a new node to the head if there is a head', () => {
-  let list = new LinkedList;
-  list.append(4);
-  list.append(2);
-  expect(list.head.next.value).toEqual(2);
-});
+  it('append() attaches a new node to the head if there is a head', () => {
+    let list = new LinkedList;
+    list.append(4);
+    list.append(2);
+    expect(list.head.next.value).toEqual(2);
+  });
 
-it('append() creates a new node with a next of null', () => {
-  let list = new LinkedList;
-  list.append(4);
-  list.append(2);
-  expect(list.head.next.next).toBe(null);
-});
+  it('append() creates a new node with a next of null', () => {
+    let list = new LinkedList;
+    list.append(4);
+    list.append(2);
+    expect(list.head.next.next).toBe(null);
+  });
 
-it('prepend() creates a new head when there is no head', () => {
-  let list = new LinkedList;
-  list.prepend(4);
-  expect(list.head.value).toEqual(4);
-});
+  it('prepend() creates a new head when there is no head', () => {
+    let list = new LinkedList;
+    list.prepend(4);
+    expect(list.head.value).toEqual(4);
+  });
 
-it('prepend() attaches a new node to the head if there is a head', () => {
-  let list = new LinkedList;
-  list.prepend(4);
-  list.prepend(2);
-  expect(list.head.value).toEqual(2);
-  expect(list.head.next.value).toEqual(4);
-});
+  it('prepend() attaches a new node to the head if there is a head', () => {
+    let list = new LinkedList;
+    list.prepend(4);
+    list.prepend(2);
+    expect(list.head.value).toEqual(2);
+    expect(list.head.next.value).toEqual(4);
+  });
 
-it('prepend() changes the next of the original head to null', () => {
-  let list = new LinkedList;
-  list.prepend(4);
-  list.prepend(2);
-  expect(list.head.next.next).toBe(null);
-});
+  it('prepend() changes the next of the original head to null', () => {
+    let list = new LinkedList;
+    list.prepend(4);
+    list.prepend(2);
+    expect(list.head.next.next).toBe(null);
+  });
 
-it('reverse() does not modify the list if only one node exists', () => {
-  let list = new LinkedList;
-  list.prepend(4);
-  list.reverse();
-  expect(list.head.value).toEqual(4);
-});
+  it('reverse() does not modify the list if only one node exists', () => {
+    let list = new LinkedList;
+    list.prepend(4);
+    list.reverse();
+    expect(list.head.value).toEqual(4);
+  });
 
-it('reverse() reverses the order of a linked list with two nodes', () => {
-  let list = new LinkedList;
-  list.prepend(4);
-  list.prepend(2);
-  list.reverse();
-  expect(list.head.value).toEqual(4);
-  expect(list.head.next.value).toEqual(2);
-});
+  it('reverse() reverses the order of a linked list with two nodes', () => {
+    let list = new LinkedList;
+    list.prepend(4);
+    list.prepend(2);
+    list.reverse();
+    expect(list.head.value).toEqual(4);
+    expect(list.head.next.value).toEqual(2);
+  });
 
-it('reverse() reverses the order of a linked list with many nodes', () => {
-  let list = new LinkedList;
-  list.prepend(4);
-  list.prepend(2);
-  list.prepend(1);
-  list.reverse();
-  expect(list.head.value).toEqual(4);
-  expect(list.head.next.value).toEqual(2);
-  expect(list.head.next.next.value).toEqual(1);
+  it('reverse() reverses the order of a linked list with many nodes', () => {
+    let list = new LinkedList;
+    list.append(4);
+    list.append(2);
+    list.append(1);
+    list.reverse();
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next.value).toEqual(4);
+  });
+
 });

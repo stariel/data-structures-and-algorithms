04b45e3e519cb8c65c5450e9dc53b3a015151ac7
Ariel Altaras <ariel.altaras@gmail.com>
2018-09-11 18:29:48 -0700

    adding insertBefore function

---
+++ linked-lists/__tests__/ll_insertions_test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let LinkedList = require('../lib/ll_insertions.js');
+
+describe('Linked LinkedList Constructor', () => {
+  it('append() creates a new head when there is no head', () => {
+    let list = new LinkedList;
+    append(4);
+    expect(list.head.value).toEqual(4);
+  });
+
+  it('append() attaches a new node to the head if there is a head', () => {
+    let list = new LinkedList;
+    append(4);
+    append(2);
+    expect(list.head.next.value).toEqual(2);
+  });
+
+  it('append() creates a new node with a next of null', () => {
+    let list = new LinkedList;
+    append(4);
+    append(2);
+    expect(list.head.next.next).toBe(null);
+  })
+}
---
+++ linked-lists/lib/ll_insertions.js
@@ -0,0 +1,61 @@
+'use strict';
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+    }
+
+    else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = new Node(value);
+    }
+  }
+
+  insertBefore(val, newVal) {
+    if (!this.head) {
+      throw console.error('This list does not contain any values.');
+    }
+    else if (this.head.value == val) {
+      let save = this.head;
+      this.head = new Node(newVal);
+      this.head.next = save;
+    } else if (this.head.next !== null) {
+      let current = this.head.next;
+      let previous = this.head;
+      while (current.next !== null) {
+        if(current.value == val) {
+          let saved = current;
+          let newNode = new Node(newVal);
+          previous.next = newNode;
+          newNode.next = saved;
+        }
+        previous = current;
+        current = current.next;
+      }
+      throw console.error('This list does not contain the given value.');
+    }
+
+  }
+
+  insertAfter(val, newVal) {
+    if (!this.head) {
+      throw console.error('This list does not contain any values.');
+    }
+  }
+
+}
